32da9226cbec183a37b4ed443b7b5ca5efd509bb
mhamner <37221769+mhamner@users.noreply.github.com>
2021-06-07 16:54:35 -0400

    Add files via upload

---
+++ ConsoleAppCSharpDemoPartialClasses/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCSharpDemoPartialClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new student
+            Student student = new Student(1, "Harry Potter");
+
+            //Show the student's info:
+            student.ShowStudentInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleAppCSharpDemoPartialClasses/StudentMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCSharpDemoPartialClasses
+{
+    public partial class Student
+    {
+        //Constructor
+        public Student(int studentId, string studentName)
+        {
+            this.StudentId = studentId;
+            this.StudentName = studentName;
+        }
+
+        public void ShowStudentInfo()
+        {
+            Console.WriteLine($"Student ID: {this.StudentId}, Student Name: {this.StudentName}.");
+        }
+    }
+}
